2e5c05401aeaa16f7c21c47c9f1255c6bc5bf2f7
Berkcan Meric <berkcanmeric99@hotmail.com>
2024-09-15 16:54:14 +0300

    Versioning untracked files

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -14,5 +14,6 @@ export async function POST(req: NextRequest) {
     react: WelcomeTemplate({ name: 'Berkcan' }),
   });
 
+  
   return NextResponse.json({ message: 'Email sent successfully' });
 }
